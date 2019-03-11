59b132208990b77d35f61cf42d50c5e18709bc41
Duchess-bgd <47751331+Duchess-bgd@users.noreply.github.com>
2019-03-11 20:17:41 +0100

    11.3.

--- sveKorpeJs.js
+++ sveKorpeJs.js
@@ -1,14 +1,7 @@
-
 var sk = localStorage.getItem("sveKorpe");
-
-console.log(sk);
-document.querySelector("#Dzejson").innerHTML=sk;
-
+var sveKorpe;
 if (sk !== null) sveKorpe = JSON.parse(sk);
 else sveKorpe = [];
-console.log(sveKorpe);
-document.querySelector("#Dzejson2").innerHTML=sveKorpe;
-
 
 document.getElementById("clearAll").addEventListener("click", function(){	
     localStorage.removeItem("sveKorpe");
@@ -16,3 +9,24 @@ document.getElementById("clearAll").addEventListener("click", function(){
     document.getElementById("Dzejson").innerHTML="";
     localStorage.clear();
 });
+
+var s=0;
+for (var i=0; i<sveKorpe.length; i++){
+    var s=0;
+    //console.log(sveKorpe);
+    var jednaKorpa=document.createElement("div");
+    var suma=document.createElement("div");
+    jednaKorpa.innerHTML+="<br>"+"<b>"+(i+1)+". Kupac"+"<br>"+"</b>";
+    
+    
+    for (var j=0; j<sveKorpe[i].length; j++){
+        s=s+sveKorpe[i][j].kolicina*5;
+        jednaKorpa.innerHTML+=(j+1)+". "+sveKorpe[i][j].ime+ ", kolicina: "+sveKorpe[i][j].kolicina+"<br>";
+    }
+suma.innerHTML+="<b>"+"Ukupno suma: "+s+" evra"+"</b>";
+console.log(s);
+document.getElementById("Dzejson").appendChild(jednaKorpa);
+document.getElementById("Dzejson").appendChild(suma);
+}
+
+console.log(sk);
